2283425eba8eeec6712150d5eef670690a7ae9d7
Bhairab patra <bhrptr4@gmail.com>
2019-12-07 11:54:03 +0530

    Create sort.js

---
+++ sort.js
@@ -0,0 +1,15 @@
+var arr=[9,6,5,1,7,3];
+
+for(let i=0;i<arr.length;i++){
+
+    for(let j=0;j<arr.length - i;j++){
+
+        if(arr[j] > arr[j+1]){
+            temp= arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1]= temp;
+
+        }
+    }
+}
+console.log(arr);
